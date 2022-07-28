3fccf76bf3c90b32f6473677792b57626d6e325a
Flávio Monteiro <flavio.ms@outlook.com.br>
2022-07-28 09:59:02 -0300

    conversao

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -5,5 +5,15 @@ if (vel>60){
     console.log(`A velocidade do seu carro é: ${vel}Km/h`)
     console.log('Você ultrapassou o Limite de Velocidade, MULTADO!')
 }
+var dolar =5.26
+var real =100
+var conversao =Number (  real/dolar)
+
+console.log(`Valor do dólar U$ ${dolar}`)
+console.log(`Valor em Real R$ ${real}`)
+console.log(`Valor da conversão é: U$ ${conversao.toFixed(2)} `)
+
+
+
 
 console.log('Dirija sempre usando sinto de Segurança.')
